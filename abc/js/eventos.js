9d617c0bb84fd94e2ff2ff6d10dccb183967fc78
joselineem <joselineem@gmail.com>
2017-06-01 15:02:21 -0600

    algo gg

--- abc/js/eventos.js
+++ abc/js/eventos.js
@@ -1,8 +1,32 @@
 var iniciaApp = function(){
 	//alert("Hola App");
 	var entrar=function(){
-		alert($("#txtUsuario").val());
-		alert($("#txtClave").val());
+		var usuario= $("#txtUsuario").val();
+		var clave= $("#txtClave").val();
+		var parametros= "opcion=valida"+ 
+						"&usuario="+usuario+
+						"&clave="+clave+
+						"&id="+Math.random();
+		var validaEntrada = $.ajax({
+			method:"POST",
+			url:"php/datos.php",
+			data:parametros,
+			//recibir un json
+			dataType:"json"
+		});
+		validaEntrada.done(function(data){
+			alert(data.respuesta);
+			if(data.respuesta==true){
+				$("#datosUsuario").hide();
+				$("nav").show("slow");
+			}
+		});
+		//Por si falla, una funcion que recibe un 
+		//codigo de error o un estatus
+		validaEntrada.fail(function(jqError,textStatus){
+			alert("Solicitud fallida"+textStatus);
+		});
+
 	}
 	var teclaUsuario=function(tecla){
 		if(tecla.which==13){
--- abc/php/datos.php
+++ abc/php/datos.php
@@ -3,8 +3,8 @@
 	function valida(){
 		$respuesta=false;
 		$conexion=conecta();
-		$u=GetSQLValueString($_GET["usuario"],"text");
-		$c=GetSQLValueString(md5($_GET["clave"]),"text");
+		$u=GetSQLValueString($_POST["usuario"],"text");
+		$c=GetSQLValueString(md5($_POST["clave"]),"text");
 		$consulta=sprintf("select usuario, clave from usuarios where usuario=%s and clave=%s limit 1", $u,$c);
 		$resultado=mysql_query($consulta);
 		if(mysql_num_rows($resultado)>0){
@@ -14,7 +14,7 @@
 		print(json_encode($salidaJSON));
 	}
 	//Menú principal
-	$opcion=$_GET["opcion"];
+	$opcion=$_POST["opcion"];
 	switch ($opcion) {
 		case 'valida':
 			valida();
